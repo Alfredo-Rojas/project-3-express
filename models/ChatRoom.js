bfdc084794f16afc5a432ddf3bcea206e043d00a
Alfredo-Rojas <alfredot84@gmail.com>
2019-07-30 15:03:52 -0400

    initial commit

---
+++ models/ChatRoom.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const chatRoomSchema = new Schema({
+  title: String,
+  description: String,
+  messages: [{
+    owner: {type: Schema.Types.ObjectId, ref: 'User'},
+    timestamp: Date,
+    manssage: String,
+  }],
+  owner: {type: Schema.Types.ObjectId, ref: 'User'},
+});
+
+const ChatRoom = mongoose.model('ChatRoom', chatRoomSchema);
---
+++ models/MeetUp.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Shema    = mongoose.Schema;
+
+const meetUpSchema = new Schema({
+  title: String,
+  location: {
+    streetAddress: String,
+    city: String,
+    state: String,
+    country: String,
+  },
+  owner: {type: Schema.Types.ObjectId, ref: "User"},
+  participants: {type: Schema.Types.ObjectId, ref: "User"},
+  type: String,
+  description: String,
+  going: {type: Schema.Types.ObjectId, ref: "User"},
+  notGoing: {type: Schema.Types.ObjectId, ref: "User"},
+  checkin: [{type: Schema.Types.ObjectId , ref: 'User'}],
+  leaving: [{type: Schema.Types.ObjectId , ref: 'User'}],
+  time: Date,
+});
+
+module.exports = mongoose.model('MeetUp', meetUpSchema);
